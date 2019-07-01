8915ed5bdceb87b9b4f7db524428c56aca3a9c0b
olliketola <olliketola@live.fi>
2019-07-01 14:40:34 +0300

    updates

--- src/app/component/component.component.ts
+++ src/app/component/component.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ApplicationRef, Component, OnInit } from '@angular/core';
 import { Model } from '../repository.model';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app',
@@ -9,9 +10,18 @@ import { Model } from '../repository.model';
 export class ProductComponent implements OnInit {
 
   model: Model = new Model();
-  constructor() { }
+
+  counter: number = 1;
+
+  constructor(ref: ApplicationRef) {
+
+    (<any>window).appRef = ref;
+    (<any>window).model = this.model;
+
+  }
 
   ngOnInit() {
+
   }
 
   getClasses(key: number): string {
@@ -32,4 +42,48 @@ export class ProductComponent implements OnInit {
   fontSizeWithUnits: string = "30px";
   fontSizeWithoutUnits: string = "30";
 
+  getStyles(key: number) {
+    let product = this.model.getProduct(key);
+    return {
+      fontSize: "30px",
+      "margin.px": 100,
+      color: product.price > 50 ? "red" : "green"
+    };
+  }
+
+  getProductByPosition(position: number): Product {
+    return this.model.getProducts()[position];
+  }
+
+  getClassesByPosition(position: number): string {
+    let product = this.getProductByPosition(position);
+    return "p-2 " + (product.price < 50 ? "bg-info" : "bg-warning");
+  }
+
+  getProduct(key: number): Product {
+    return this.model.getProduct(key);
+  }
+
+  getProducts(): Product[] {
+    return this.model.getProducts();
+  }
+  
+  getProductCount(): number {
+    console.log("getProductCount invoked");
+    return this.getProducts().length;
+  }
+
+  getKey(index: number, product: Product) {
+    return product.id;
+  }
+
+  get nextProduct(): Product {
+    return this.model.getProducts().shift();
+  }
+
+  getProductPrice(index: number): number {
+    return Math.floor(this.getProduct(index).price);
+  }
+
+  targetName: string = "Kayak";
 }
